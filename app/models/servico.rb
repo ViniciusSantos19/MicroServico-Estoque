47b7bf14e227acde190a64a9989a93d1ffd02c52
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-03-20 11:41:49 -0300

    removi validador que não estava sendo usado

--- app/models/servico.rb
+++ app/models/servico.rb
@@ -1,4 +1,8 @@
 class Servico < ApplicationRecord
+  validates :nome, presence: true, length: { maximum: 55 }
+  validates :descricao, presence: true, length: { maximum: 255 }
+  validates :preco, :duracao, presence: true, numericality: { greater_than: 0 }
+
   attr_accessor :nome, :descricao, :duracao, :preco
 
   has_and_belongs_to_many :produtos
--- test/models/estoque_test.rb
+++ test/models/estoque_test.rb
@@ -8,18 +8,18 @@ class EstoqueTest < ActiveSupport::TestCase
   test 'deve ser inválido sem quantidade' do
     estoque = Estoque.new(data_entrada: Date.today, data_saida: Date.tomorrow)
     assert_not estoque.valid?
-    assert_includes estoque.errors.full_messages, 'Quantidade não pode ficar em branco'
+    assert_includes estoque.errors.full_messages, "Quantidade can't be blank"
   end
 
   test 'deve ser inválido sem data de entrada' do
     estoque = Estoque.new(quantidade: 10, data_saida: Date.tomorrow)
     assert_not estoque.valid?
-    assert_includes estoque.errors.full_messages, 'Data de entrada não pode ficar em branco'
+    assert_includes estoque.errors.full_messages, "Data entrada can't be blank"
   end
 
   test 'deve ser inválido sem data de saída' do
     estoque = Estoque.new(quantidade: 10, data_entrada: Date.today)
     assert_not estoque.valid?
-    assert_match(/Quantidade não pode ficar em branco/, estoque.errors.full_messages.join)
+    assert_includes estoque.errors.full_messages, "Data saida can't be blank"
   end
 end
