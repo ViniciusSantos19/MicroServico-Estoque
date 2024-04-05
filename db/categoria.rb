10a3c7a9cedd91d40caad00570973c39c6cf7f2c
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-04-05 11:20:02 -0300

    adicionei um arquivo de docker-compose.yml

--- db/categoria.rb
+++
@@ -1,8 +0,0 @@
-require 'app/models/categorium'
-
-Categorium.create(nome: 'barba', descricao: 'produtos para a barba do cliente')
-Categorium.create(nome: 'Cabelo', descricao: 'produtos para os cabelos do cliente')
-Categorium.create(nome: 'Máquina', descricao: 'máquina de cortar cabelo')
-Categorium.create(nome: 'Navalha', descricao: 'navalha para alinhar pé do cabelo')
-
-puts "Criadas #{Categorium.count} categorias."
